d95f13fab6e8a0ed80011fba8f5c5e64acc2a0d8
Niels Heltner <nhelt15@student.sdu.dk>
2019-02-13 17:43:34 +0100

    changed List to Map

--- src/mdsd/model/MetaModel.java
+++ src/mdsd/model/MetaModel.java
@@ -1,7 +1,10 @@
 package mdsd.model;
 
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -14,29 +17,36 @@ import java.util.Map;
 public class MetaModel {
 	
 	/**
-	 * Lookup table for the microservices currently in the system. // THIS SHOULD BE MOVED TO THE FLUENT INTERFACE FOR BINDING
+	 * Lookup table for the microservices currently in the system.
 	 */
-	//private Map<String, Microservice> microservices;
-	
-	/**
-	 * Collection of the microservices in the system.
-	 */
-	private List<Microservice> microservices;
+	private Map<URL, Microservice> microservices;
 	
 	/**
 	 * Construct
 	 * @param microservices the collection of microservices the system consists of
 	 */
 	public MetaModel(List<Microservice> microservices) {
-		this.microservices = new ArrayList<Microservice>(microservices); 
+		this.microservices = new HashMap<URL, Microservice>();
+		for (Microservice microservice : microservices) {
+			this.microservices.put(microservice.getUrl(), microservice);
+		}
 	}
 	
 	/**
 	 * Gets a read-only collection of the microservices this system consists of.
 	 * @return
 	 */
-	public List<Microservice> getMicroservices() {
-		return Collections.unmodifiableList(microservices);
+	public Collection<Microservice> getMicroservices() {
+		return Collections.unmodifiableCollection(microservices.values());
+	}
+	
+	/**
+	 * Get a specific microservice
+	 * @param url the url corresponding to the microservice to rectrieve
+	 * @return
+	 */
+	public Microservice getMicroservice(URL url) {
+		return microservices.get(url);
 	}
 
 }
