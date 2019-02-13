729dbe5b1f478a7522ae1e37be952ca6cac71beb
Niels Heltner <nhelt15@student.sdu.dk>
2019-02-13 16:57:39 +0100

    added builder class for fluent interface

---
+++ src/mdsd/dsl/MicroserviceBuilder.java
@@ -0,0 +1,53 @@
+package mdsd.dsl;
+
+import mdsd.model.HttpMethod;
+import mdsd.model.MetaModel;
+
+/**
+ * Fluent interface, representing an internal DSL, for populating the MetaModel.
+ * Follows the Expression Builder pattern [Fowler].
+ * 
+ * @author Niels
+ *
+ */
+public abstract class MicroserviceBuilder {
+	
+	private MetaModel metaModel;
+	
+	public MetaModel getMetaModel() {
+		buildAndVerifyModel();
+		return metaModel;
+	}
+	
+	private void buildAndVerifyModel() {
+		build();
+		// verify and handle illegal states and errors
+	}
+
+	/**
+	 * Override in subclasses, must define populate meta model 
+	 * using fluent interface
+	 */
+	protected abstract void build();
+	
+	public MicroserviceBuilder microservice(String name, String location, int port) {
+		return this;
+	}
+	
+	public MicroserviceBuilder endpoint(String name) {
+		return this;
+	}
+	
+	public MicroserviceBuilder method(HttpMethod method) {
+		return this;
+	}
+	
+	public MicroserviceBuilder parameter(String name, Class<?> type) {
+		return this;
+	}
+	
+	public MicroserviceBuilder response(Class<?> type) {
+		return this;
+	}
+
+}
