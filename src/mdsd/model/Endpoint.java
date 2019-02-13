155ab795201367e4f5e1fa36241792b9e7ce0cad
Niels Heltner <nhelt15@student.sdu.dk>
2019-02-13 15:57:52 +0100

    added comments to Endpoint class

--- src/mdsd/model/Endpoint.java
+++ src/mdsd/model/Endpoint.java
@@ -4,16 +4,45 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Represents an endpoint for a microservice. Consists of a valid HttpMethod, a collection of 
+ * expected parameter names and their types, and a response type.
+ * 
+ * @author Niels
+ *
+ */
 public class Endpoint {
 	
+	/**
+	 * The name of this endpoint, which corresponds to the path in the URL.
+	 */
+	private String name;
+	
+	/**
+	 * The valid HttpMethod for this endpoint.
+	 */
 	private HttpMethod method;
 	
+	/**
+	 * The parameters this endpoint expects to receive. Consists of key-value pairs
+	 * of the parameter's name and the parameter's type.
+	 */
 	private Map<String, Class<?>> parameters;
 	
-	private Class<?> responseType = Void.class;
+	/**
+	 * The type of this endpoint's response.
+	 * Defaults to Void.class if none is given.
+	 */
+	private Class<?> responseType;
 	
-	public Endpoint() {
+	/**
+	 * Construct
+	 * @param name the name of this endpoint
+	 */
+	public Endpoint(String name) {
+		this.name = name;
 		parameters = new HashMap<String, Class<?>>();
+		responseType = Void.class;
 	}
 	
 	public void setHttpMethod(HttpMethod method) {
@@ -36,6 +65,11 @@ public class Endpoint {
 		return responseType;
 	}
 	
+	/**
+	 * Gets the read-only collection of key-value pairs that describe the expected parameters
+	 * for this endpoint.
+	 * @return
+	 */
 	public Map<String, Class<?>> getParameters() {
 		return Collections.unmodifiableMap(parameters);
 	}
